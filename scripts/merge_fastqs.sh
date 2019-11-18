760a5183155a8f39214f7d1d01ebd00a89f8f183
Inés Rodríguez <inesrgarcia31@gmail.com>
2019-11-18 21:05:29 +0100

    script merge_fastqs.sh más más definitivo

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -3,4 +3,7 @@
 # The directory containing the samples is indicated by the first argument.
 
 mkdir -p $2
-cat $1/$3-12.5dpp.1.1s_sRNA.fastq.gz data/$3-12.5dpp.1.2s_sRNA.fastq.gz > $2/$3.fastq.gz 
+cat $1/$3*.fastq.gz  > $2/$3.fastq.gz
+
+
+
