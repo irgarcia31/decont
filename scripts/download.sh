c230959dc8d18e590467bc602e663ed5c0249858
Inés Rodríguez <inesrgarcia31@gmail.com>
2019-11-18 19:12:08 +0100

    script download.sh aparentemente correcto

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,10 +1,10 @@
 # This script should download the file specified in the first argument ($1), place it in the directory specified in the second argument, 
 # and *optionally* uncompress the downloaded file with gunzip if the third argument contains the word "yes".
 
-if [$3 == 'yes']
+if [ "$3" == "yes" ]
 then
-	wget -O $2 $1
+	wget -P $2 $1
 	gunzip -k $2
 else
-	wget -O $2 $1
+	wget -P $2 $1
 fi
