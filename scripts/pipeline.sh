8a2c24354997e9157e673ae3d2c9b511d2dd608c
Inés Rodríguez <inesrgarcia31@gmail.com>
2019-11-19 20:53:40 +0100

    script pipeline.sh hasta STAR

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -17,16 +17,22 @@ do
     bash scripts/merge_fastqs.sh data out/merged $sid
 done
 
-# TODO - for: run cutadapt for all merged files
-# cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed -o <trimmed_file> <input_file> > <log_file>
 
-#TODO: run STAR for all trimmed files
+# Run cutadapt for all merged files
+mkdir -p out/trimmed
+mkdir -p log/cutadapt
+for sid in $(ls out/merged/*.fastq.gz | cut -d "." -f1 | sed 's:out/merged/::' | sort | uniq)
+do
+	cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed -o  out/trimmed/${sid}.trimmed.fastq.gz out/merged/${sid}.fastq.gz > log/cutadapt/${sid}.log
+done
+
+# Run STAR for all trimmed files
 for fname in out/trimmed/*.fastq.gz
 do
     # you will need to obtain the sample ID from the filename
-    sid=#TODO
-    # mkdir -p out/star/$sid
-    # STAR --runThreadN 4 --genomeDir res/contaminants_idx --outReadsUnmapped Fastx --readFilesIn <input_file> --readFilesCommand zcat --outFileNamePrefix <output_directory>
+    sid=$(basename $fname .trimmed.fastq.gz)
+    mkdir -p out/star/$sid
+	STAR --runThreadN 4 --genomeDir res/contaminants_idx --outReadsUnmapped Fastx --readFilesIn out/trimmed/${sid}.trimmed.fastq.gz --readFilesCommand zcat --outFileNamePrefix out/star/${sid}/
 done 
 
 # TODO: create a log file containing information from cutadapt and star logs
